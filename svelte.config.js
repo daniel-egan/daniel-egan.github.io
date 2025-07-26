f0b860d96def0ba7b07eb80ca744b0503e4851a9
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-07-26 11:28:20 +0100

    More updates

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
