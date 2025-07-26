4145bc12397355bc0687e6d8064f923447295a66
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2025-07-26 12:22:13 +0100

    Adding stuff

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	paths: {
+		base: process.env.NODE_ENV === 'production' ? '/daniel-egan.github.io' : ''
+	}
 };
 
 export default config;
